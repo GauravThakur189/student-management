d36a6983ab18287717b80e55186733b085f67de9
Gaurav Singh <gauravsingh1892000@gmail.com>
2024-08-16 15:20:29 +0530

    crud operation completed in this project

--- student-management-system/src/main/java/net/javaproject/sms/controller/StudentController.java
+++ student-management-system/src/main/java/net/javaproject/sms/controller/StudentController.java
@@ -75,4 +75,12 @@ public class StudentController {
          return "redirect:/students";
 
     }
+
+    @GetMapping("/students/{studentId}/view")
+    public String viewStudent(@PathVariable("studentId") Long studentId, Model model){
+        StudentDto studentDto = studentService.getStudentById(studentId);
+        model.addAttribute("student",studentDto);
+        return "view_student";
+
+    }
 }
